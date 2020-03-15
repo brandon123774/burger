a9b17062657f0653bbe321e7c638aa86f5741576
Brandon Shankar <bshankar12@comcast.net>
2020-03-15 14:01:32 -0700

    error on server.js fixed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 INSERT INTO burgers (burger_name) VALUES ('Spicy Chicken');
 INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
-INSERT INTO burgers (burger_name, eaten) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
 
